7c29f454dd42bce1b45ad66b741c5d00e5e711cd
Ananda Ricky Fauzi <anandaricky01@gmail.com>
2023-01-01 15:50:11 +0700

    Create sivoka.js

---
+++ sivoka.js
@@ -0,0 +1,9 @@
+function isi(tanggal){
+    $("#id_bimbing1").val("BAYU SUTAWIJAYA, M.KOM, S.Kom. ").change();
+    $("#id_tmpmagang").val("Bangbeli").change();
+    $("#id_tanggal").val(`${tanggal}`).change();
+    $("#id_jam1").val("9").change();
+    $("#id_jam2").val("17").change();
+    $("#id_menit1").val("0").change();
+    $("#id_menit2").val("0").change();
+}
